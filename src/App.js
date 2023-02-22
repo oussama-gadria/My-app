66ca8a0d9d5a7b7814f17bb058410d501bee5c85
Oussama Gadria <gadria.oussama@esprit.tn>
2023-02-22 01:32:31 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Products from './composants/Products';
 import ProductsFunction from './composants/ProductsFunctions';
 function App() {
   return (
--- src/composants/Products.js
+++ src/composants/Products.js
@@ -1,43 +1,44 @@
-import Product from "./Product";
+import Product from './Product';
 import React, { Component } from 'react';
-import products from '../products.json'
+import products from '../products.json';
+
 class Products extends Component {
+  state = {
+    message: '',
+    variant: null,
+  };
 
-    state = {  
-        message:"",
-        variant:null
+  decrementquantite = (product) => {
+    if (product.quantity > 0) {
+      product.quantity--;
+      this.setState({
+        message: 'You Bought an item',
+        variant: 'success',
+      });
+      setTimeout(() => {
+        this.setState({
+          message: '',
+          variant: null,
+        });
+      }, 2000);
     }
-    decrementquantite=(product)=>{ 
-       
-         if (product.quantite>0){ 
-           product.quantity -- ; 
-           this.setState({ 
-             message:"You Bought an item",
-             variant:"success"
-         })
-         setTimeout(()=>{ 
-           this.setState({ 
-             message:"",
-             variant:null
-         })
-         },2000)
-         }
-        
-      }
-    render() { 
-        return (
-        <>
-        { 
-            products.map((p)=>{         
-                return(<Product product={p}  parentproduct={this.decrementquantite}></Product>)
-        }
-        )
-    }    
-   
-        </>)
-       
-    }
+  };
 
+  render() {
+    return (
+      <>
+        {products.map((p) => {
+          return (
+            <Product
+              product={p}
+              parentproduct={this.decrementquantite}
+              key={p.id}
+            />
+          );
+        })}
+      </>
+    );
+  }
 }
- 
+
 export default Products;
